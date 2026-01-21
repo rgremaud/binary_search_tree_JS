b38b5a46b3e1b0ac22c1a198d136e9ce91b47cfb
rgremaud <rgremau@gmail.com>
2026-01-20 21:59:47 -0500

    Add initial BST function

--- tree.js
+++ tree.js
@@ -1,33 +1,68 @@
+import { Node } from "./node.js"
 /*
 Build a Tree class/factory which accepts an array when initialized. The Tree 
 class should have a root attribute, which uses the return value of buildTree 
 which you’ll write next.
+*/
+export class Tree { 
+  constructor(array) {
+    this.array = this.trimArray(array);
+    this.root = null;
+  }
 
+  trimArray(array) {
+    const formattedArray = [...new Set(array)];
+    formattedArray.sort((a, b) => a - b);
 
+    return formattedArray;
+  }
+
+/*
 Write a buildTree(array) function that takes an array of data 
 (e.g., [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a 
 balanced binary tree full of Node objects appropriately placed 
 (don’t forget to sort and remove duplicates!). 
 The buildTree function should return the level-0 root node.
+*/
+  sortedArrayToBSTRecur(array, start, end) {
+    if (start > end) return null;
 
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(array[mid]);
+
+    // Divide from middle element
+    root.left = this.sortedArrayToBSTRecur(array, start, mid - 1);
+    root.right = this.sortedArrayToBSTRecur(array, mid + 1, end);
+
+    this.root = root
+    return root;
+  }
+
+  buildTree(array) {
+    return this.sortedArrayToBSTRecur(array, 0, array.length - 1);
+  }
+
+/*
 Tip: If you would like to visualize your binary search tree, here is a 
 prettyPrint() function that will console.log your tree in a structured format. 
 This function will expect to receive the root of your tree as the value for the 
 node parameter.
+*/
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
+prettyPrint(node, prefix = '', isLeft = true) {
   if (node === null) {
     return;
   }
   if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
   }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
   if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
 };
 
+/*
 Write insert(value) and deleteItem(value) functions that insert/delete the given
  value. You’ll have to deal with several cases for delete, such as when a node 
  has children or not. If you need additional resources, check out these two 
@@ -66,3 +101,5 @@ should traverse the tree in their respective depth-first order and pass each nod
  Write a rebalance function that rebalances an unbalanced tree. 
  Tip: You’ll want to use a traversal method to provide a new array to the buildTree function.
 */
+
+}
